cd613d312e567e89b53904c6dfdfc7b79e36cbd1
Maddievanh <mv180818@umconnect.umt.edu>
2022-12-14 11:29:59 -0700

    Final Game

--- Final/sketch.js
+++ Final/sketch.js
@@ -12,11 +12,26 @@ function setup() {
 }
 
 function draw() {
- if(screen == 0){
+if(screen== 0){
+    welcomeScreen()
+}
+ else if(screen == 1){
   	startGame()
   }else if(screen==2){
   	gameOver()
   }	
+}
+
+//WELCOME
+function welcomeScreen(){
+    background(0);
+    fill(204,255,255);
+    textSize(35);
+    textAlign(CENTER);
+    text('Happy Finals! ', width/ 2, height /2)
+    textSize(10); 
+    text ('click screen to play the game', width/ 2, height /2 + 20)
+
 }
 
 //game screen
@@ -43,22 +58,31 @@ function startGame(){
     score+= 1
   }
 	if(y==-20){
-  	pickRandom();
+  	setRandom();
   }
 }
 
-function pickRandom(){
+function setRandom(){
 	x= random(20,width-20)
 }
-
+//change screen 0-1 
+function mousePressed(){
+	if(screen==0){
+  	screen=1
+  }else if(screen==2){
+  	screen=0
+  }
+}
 //game over screen 
 function gameOver(){
 		background(62,12,94); 
         fill(255);
+        textSize(25); 
 		textAlign(CENTER);
         textStyle(BOLD);
 		text('SORRY :( YOU MISSED', width / 2, height / 2)
-  	text("GAME OVER",  width / 2, height / 2 + 20)
+        textSize(15); 
+  	text("GAME OVER",  width / 2, height / 2 + 30)
 		
 }
 
